0acede10d9b67d23ffc4f58d7e9a94b28ab292b6
Mohamd977 <mohammad.you.977@gmail.com>
2025-04-19 17:44:10 +0300

    -

--- App.tsx
+++ App.tsx
@@ -1,5 +1,37 @@
+import { View, Text } from 'react-native';
 import Navigation from './navigation';
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import React, { useState, useEffect } from 'react';
+import { app } from './firebase/firebaseConfig';
+import { NavigationContainer } from '@react-navigation/native';
+import Auth from './components/pages-ui/auth/Auth';
 
 export default function App() {
-  return <Navigation />;
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth(app);
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    // Cleanup subscription
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      {user ? <Navigation /> : <Auth />}
+    </NavigationContainer>
+  );
 }
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,10 +1,9 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import AboutScreen from '../screens/AboutScreen';
 import IdInfoScreen from '../screens/IdInfoScreen';
 import AddIdData from '../screens/AddIdData';
-import LinkBtn from '../components/ui/about-us/LinkBtn';
+
 export type RootStackParamList = {
   Home: undefined;
   About: undefined;
@@ -16,13 +15,11 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function Navigation() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" options={{ title: "هويتي", headerTitleAlign: "center", headerRight: () => <LinkBtn /> }} component={HomeScreen} />
-        <Stack.Screen name="IdInfo" options={{ title: "هويتي الرقمية" }} component={IdInfoScreen} />
-        <Stack.Screen name="AddIdData" options={{ title: "إضافة البيانات" }} component={AddIdData} />
-        <Stack.Screen name="About" options={{ title: "عن التطبيق" }} component={AboutScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" options={{title: "هويتي"}} component={HomeScreen} />
+      <Stack.Screen name="IdInfo" options={{title: "هويتي الرقمية"}} component={IdInfoScreen} />
+      <Stack.Screen name="AddIdData" options={{title: "إضافة البيانات"}} component={AddIdData} />
+      <Stack.Screen name="About" options={{title: "عن التطبيق"}} component={AboutScreen} />
+    </Stack.Navigator>
   );
 }
